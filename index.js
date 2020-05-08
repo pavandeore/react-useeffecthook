74891d0eb924b35a15dceb62856ed98110522bf0
pavandeore <pavandeore90@gmail.com>
2020-05-08 14:28:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
 class App extends Component {
   constructor() {
+
+    const [growth, setGrowth] = useState(0);
+
     super();
     this.state = {
       name: 'React'
